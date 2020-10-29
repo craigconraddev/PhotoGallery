623482e8429d4340faea4a86fa009bbcfd60f16d
curion4 <curion4@gmail.com>
2020-10-28 21:38:44 -0400

    Chapter24 Challenge 1  Adding a custom Gson Deserializer

--- app/src/main/java/com/bignerdranch/android/photogallery/FlickrFetcher.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/FlickrFetcher.kt
@@ -6,12 +6,13 @@ import androidx.lifecycle.MutableLiveData
 import com.bignerdranch.android.photogallery.api.FlickrApi
 import com.bignerdranch.android.photogallery.api.FlickrResponse
 import com.bignerdranch.android.photogallery.api.PhotoResponse
+import com.google.gson.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import java.lang.reflect.Type
 
 private const val TAG = "FlickrFetcher"
 
@@ -20,9 +21,12 @@ class FlickrFetcher {
     private val flickrApi: FlickrApi
 
     init {
+        val gson : Gson = GsonBuilder()
+            .registerTypeAdapter(PhotoResponse::class.java, PhotoDeserializer())
+            .create()
         val retrofit: Retrofit = Retrofit.Builder()
             .baseUrl("https://api.flickr.com/")
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
 
         flickrApi = retrofit.create(FlickrApi::class.java)
@@ -51,5 +55,28 @@ class FlickrFetcher {
             }
         })
         return responseLiveData
+    }
+
+    class PhotoDeserializer: JsonDeserializer<PhotoResponse> {
+        override fun deserialize(
+            json: JsonElement?,
+            typeOfT: Type?,
+            context: JsonDeserializationContext?
+        ): PhotoResponse {
+            val photoObject = json?.asJsonObject!!
+            val galleryItems = mutableListOf<GalleryItem>()
+            val photoArray = photoObject.get("photo").asJsonArray
+            photoArray.forEach { photo ->
+                val galleryItem = GalleryItem()
+                val photoElement = photo.asJsonObject
+                galleryItem.title = photoElement.get("title").asString;
+                galleryItem.id = photoElement.get("id").asString;
+                galleryItem.url = photoElement.get("url_s").asString;
+                galleryItems.add(galleryItem)
+            }
+           val photoResponse = PhotoResponse()
+            photoResponse.galleryItems = galleryItems
+            return photoResponse
+        }
     }
 }
